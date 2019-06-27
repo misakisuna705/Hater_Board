375e7030cf247b1cf6db8c2d7e0a9a32e2ca0fa8
misakisuna705 <misakisuna705@gmail.com>
2019-06-27 14:04:51 +0800

    commit

--- dist/res/js/config.js
+++
@@ -1,1 +0,0 @@
-var config={apiKey:"AIzaSyCIg9T5jx3bzpgIClMjnNIGRD8Wd0gfrfI",authDomain:"hater-board.firebaseapp.com",databaseURL:"https://hater-board.firebaseio.com",projectId:"hater-board",storageBucket:"hater-board.appspot.com",messagingSenderId:"528205002613"};firebase.initializeApp(config);
